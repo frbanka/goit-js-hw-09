07e8784add1bf4ec0304506acb4690e497abd72d
frbanka <109819901+frbanka@users.noreply.github.com>
2023-01-17 19:41:34 +0000

    Deploying to gh-pages from @ frbanka/goit-js-hw-09@e7492a1d48ca8c1d6ee74905ba23e3c878cbf7a0 🚀

---
+++ 01-color-switcher.13654ae1.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("data-start"),document.querySelectorAll("data-stop");
+//# sourceMappingURL=01-color-switcher.13654ae1.js.map
---
+++ 01-color-switcher.d5797e3d.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("data-start"),document.querySelectorAll("data-stop");
+//# sourceMappingURL=01-color-switcher.d5797e3d.js.map
