e7492a1d48ca8c1d6ee74905ba23e3c878cbf7a0
Jacek Wójciak <fr.banka@gmail.com>
2023-01-17 20:41:12 +0100

    up parcel

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,6 @@
+const startButton = document.querySelectorAll('data-start');
+const stopButton = document.querySelectorAll('data-stop');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
