c3f36ed77c7751daa81613b353746e5b89e16796
Jacek Wójciak <fr.banka@gmail.com>
2023-01-17 20:56:30 +0100

    test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,12 @@
-const startButton = document.querySelectorAll('data-start');
-const stopButton = document.querySelectorAll('data-stop');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const pageBody = document.querySelector('body');
+
+startButton.addEventListener('click', testFunc);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function testFunc() {
+  window.alert('woooow');
 }
