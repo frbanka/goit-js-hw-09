8e5933067d96390d519baec0d1623414d6b0a4d9
Jacek Wójciak <fr.banka@gmail.com>
2023-01-22 12:41:19 +0100

    work 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const firstDelay = document.querySelector('[name="delay"]');
+const stepDelay = document.querySelector('[name="step"]');
+const delayAmount = document.querySelector('[name="amount"]');
+const subBtn = document.querySelector('.submit');
+subBtn.addEventListener('click', submitPromise());
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function submitPromise(e) {
+  e.preventDefault();
+  let delay = firstDelay.valueAsNumber;
+  let step = stepDelay.valueAsNumber;
+  let amount = delayAmount.valueAsNumber;
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 }
